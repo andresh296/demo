fbffcb7fbe49cfb8c179b4716f48936b998bf951
Andres Henao <johndoe@example.com>
2023-04-12 16:56:02 -0500

    Add saludo controller

--- src/main/java/com/example/demo/controller/SaludoController.java
+++ src/main/java/com/example/demo/controller/SaludoController.java
@@ -4,10 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class PingController {
-
-    @GetMapping("/ping")
-    public String ping() {
-        return "pong";
+public class SaludoController {
+    @GetMapping("/hola")
+    public String saludo() {
+        return "Hola Mundo";
     }
 }
